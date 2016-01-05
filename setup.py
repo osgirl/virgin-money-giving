2973fad45de5a9edb53b6a852b09d2a5557b4215
James <james.bowden@bynd.com>
2016-01-05 12:47:16 +0000

    add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name='virgin-money-giving',
+    version='0.0',
+    description='Virgin Money Giving API Client',
+    author='James Bowden',
+    author_email='james.bowden@bynd.com',
+    url='',
+    packages=['virgin_money_giving'],
+    install_requires=[
+        'requests==2.9.0'
+    ]
+)
